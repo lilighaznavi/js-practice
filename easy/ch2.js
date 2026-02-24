3d77a893a73cd20252ba5c23591479fa9b353e21
LiliGhaznavi <152170657+LiliGhaznavi@users.noreply.github.com>
2026-02-24 06:22:00 -0800

    easy: update ch2 - two sum

--- easy/ch2.js
+++ easy/ch2.js
@@ -30,7 +30,7 @@ Only one valid answer exists.
 const twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
+      if (nums[i] + nums[j] === target && i !== j) {
         return [i, j];
       }
     }
